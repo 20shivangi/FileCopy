d63c3cd75bb177d4f3362ac99818d2943a749c80
20shivangi <45754171+20shivangi@users.noreply.github.com>
2021-01-20 16:07:23 +0530

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,5 @@
+[string[]]$fileContent = Get-Content ".\sample.yml"
+$content = ''
+foreach ($line in $fileContent) { $content = $content + "`n" + $line }
+$yaml = ConvertFrom-YAML $content
+Write-Host ("Account name is " + $yaml.ghaeconfig.account + ". Location is " + $yaml.ghaeconfig.location + ". Instance size is " + $yaml.ghaeconfig.instanceSize + ". Ring is " + $yaml.ghaeconfig.ring)
