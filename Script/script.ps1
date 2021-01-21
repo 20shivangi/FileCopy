e9aa1b060663a453d637db233d880e1f7af8f486
20shivangi <45754171+20shivangi@users.noreply.github.com>
2021-01-21 15:40:22 +0530

    Update script.ps1

--- Script/script.ps1
+++ Script/script.ps1
@@ -1,4 +1,4 @@
-[string[]]$fileContent = Get-Content "$PSScriptRoot\sample.yml"
+[string[]]$fileContent = Get-Content "$PSScriptRoot\microsoft.yml"
 $content = ''
 foreach ($line in $fileContent) { $content = $content + "`n" + $line }
 $yaml = ConvertFrom-YAML $content
