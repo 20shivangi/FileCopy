65a106013c2dfa94c0c3878928e2946da6643f56
20shivangi <45754171+20shivangi@users.noreply.github.com>
2021-01-21 15:37:34 +0530

    Delete script.ps1

--- script.ps1
+++
@@ -1,5 +0,0 @@
-[string[]]$fileContent = Get-Content "$PSScriptRoot\sample.yml"
-$content = ''
-foreach ($line in $fileContent) { $content = $content + "`n" + $line }
-$yaml = ConvertFrom-YAML $content
-Write-Host ("Account name is " + $yaml.ghaeconfig.accountName + ". Location is " + $yaml.ghaeconfig.location + ". Email is " + $yaml.ghaeconfig.email + ". UserName is " + $yaml.ghaeconfig.userName)
